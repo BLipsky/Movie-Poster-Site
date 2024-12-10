3cac89e13bc3375405a8416d080808c9cd6ed5ac
BLipsky <BenLipsky@icloud.com>
2024-12-10 09:31:20 -0500

    commit 1

--- site.js
+++ site.js
@@ -5,8 +5,9 @@
 /* NOTE: MOVIES.JSON CONTAINS A LIST OF MOVIES AND ACCOMPANYING METADATA
 //
 //    They are in the following format:
-//    title (String): the name of the movie
-//    iscore (Number): the IMDB score
+//    title (String): the name of the movie */
+
+/*    iscore (Number): the IMDB score
 //    rating (String): the movie's MPAA rating
 //    released (Array): the release date. Note that the order of the array is:  YYYY, MM, DD
 //    country (String): the country of production
@@ -20,26 +21,28 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
-
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
-      },
-      data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
-      }
-    },
-      methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
-})
+  // This automatically imports your movies.json file and puts it into
+  //   the variable: movies
+  created() {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((json) => {
+        this.movies = json;
+      });
+  },
+  data() {
+    return {     
+       movies: [],
+          /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+      title: "IMDB + YourName's Top 8 Movies",
+      owner: "Ben",
+      github: "https://github.com/Gkilgore/NJIT-3_StarterFiles",
+    };
+  },
+  methods: {
+    /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+  },
+});
 
-vue_app.mount("#vue_app")
+vue_app.mount("#vue_app");
